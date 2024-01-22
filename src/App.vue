506619b969fc1657c5cabbe757b7c38c0f849f15
SunDeZhi <sundezhi@tendzone.com>
2024-01-22 18:03:14 +0800

    feature: add ElementUI

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <template>
   <div id="app">
-     <Header/>
-     <Dashboard/>
+    <Header/>
+    <Navigation/>
+    <el-button type="text" @click="show">点击打开 Message Box</el-button>
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
-import Dashboard from "@/components/Dashboard.vue";
+import Navigation from "@/components/Navigation.vue";
+
 export default {
   name: 'App',
   components: {
-     Header,
-     Dashboard
+    Header,
+    Navigation
+  },
+  methods:{
+    show(){
+      this.$notify({
+        title: '警告',
+        message: '这是一条警告的提示消息',
+        type: 'warning'
+      });
+    }
+  },
+  mounted() {
+    console.log(this)
   }
 }
 </script>
