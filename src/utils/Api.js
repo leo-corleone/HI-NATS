585e838c221cdb215191cf469421106fd91c2c31
suen <1499718389@qq.com>
2024-02-29 23:05:44 +0800

    refactor:  disconnection after finishing to close tab

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,17 +1,45 @@
 import axios from "axios";
 export class Api {
-    constructor(schema) {
-        console.log('schema' , schema)
+    constructor(prefix , schema) {
+        this.prefix = prefix;
         this.schema = schema;
     }
 
-    get = async (url, params) => {
+    get = async (uri, params) => {
         return await axios.request({
             method: "get",
-            url,
+            url: `${this.prefix}://${this.schema}${uri}`,
             params,
+            timeout: 10 * 1000
         }).then(response => response.data)
             .catch(err => err);
     }
 
+    post = async (uri, params) => {
+        return await axios.request({
+            method: "post",
+            url: `${this.prefix}://${this.schema}${uri}`,
+            params,
+            timeout: 10 * 1000
+        }).then(response => response.data)
+            .catch(err => err);
+    }
+
+    queryGeneral = async () => {
+        return await this.get("/varz")
+    }
+
+    queryConnections = async () => {
+        return await this.get("/connz")
+    }
+
+    queryHealthProbe = async () => {
+        return await this.get("/healthz")
+    }
 }
+
+
+const api = new Api("http" ,'110.41.3.32:822')
+console.log(await api.queryGeneral());
+console.log(await api.queryConnections());
+console.log(await api.queryHealthProbe());
