b43ccd49df7b8ab08dd24bc1c125655c1d7f47d8
suen <1499718389@qq.com>
2024-05-01 22:44:00 +0800

    feature； add sub/unsub button

--- src/components/main/panel/control/Subscription.vue
+++ src/components/main/panel/control/Subscription.vue
@@ -1,61 +1,83 @@
 <template>
- <div class="subscription-warp" :style="{'border-radius': '15px','border':`1px solid ${subscription.color}`}">
-   <span class="ellipsis subscription-ident" :style="'background-color:' + subscription.color"></span>
-   <span class="ellipsis subscription-content" :style=" 'color:' + subscription.color">{{subscription.topic}}</span>
-<!--   <span class="subscription-delete el-icon-collection-tag" :style="{color: subscription.isSub ? 'red' : 'green'}" v-show="isActive" @click="switchSubscribe"></span>-->
-   <span class="subscription-delete el-icon-close" @click="removeSubscription"></span>
- </div>
+    <div class="subscription-warp"
+         @mouseover="hover = true"
+         @mouseleave="hover = false"
+         :style="{'border-radius': '10px','background-color':`${subscription.color}1a`}">
+      <span class="ellipsis subscription-ident" :style="'background-color:' + subscription.color"></span>
+      <span class="ellipsis subscription-content"
+            :style=" 'color:' + subscription.color">{{ subscription.topic }}</span>
+      <span>
+        <el-checkbox :disabled="!isActive" type="success"  size="mini" circle :checked="subscription.isSub" @change="changeSubscribe(subscription.id ,!subscription.isSub)" ></el-checkbox>
+      </span>
+      <span class="subscription-remove el-icon-minus" v-if="hover" @click="removeSubscription"></span>
+    </div>
 </template>
 
 <script>
 export default {
   name: "Subscription",
-  props:['subscription' , 'removeSubscribe' , 'isActive'],
-  methods:{
-    removeSubscription(){
+  props: ['subscription', 'removeSubscribe', 'switchSubscribe' ,'isActive'],
+  data(){
+    return {
+      hover: false
+    }
+  },
+  methods: {
+    removeSubscription() {
       this.removeSubscribe(this.subscription);
     },
-    switchSubscribe(){
-
+    changeSubscribe(sId ,isSub){
+      this.switchSubscribe(sId , isSub);
     }
   }
 }
 </script>
 
 <style scoped>
-  .subscription-warp{
-    width: calc(100% - 2px);
-    height: 40px;
-    line-height: 40px;
-    margin-bottom: 1px;
-    border-radius: 20px;
-    border: 1px #837e7e solid;
-  }
+.subscription-warp {
+  width: calc(100% - 22px);
+  height: 40px;
+  line-height: 40px;
+  margin: 3px auto 2px;
+  border-radius: 10px;
+}
 
-  .subscription-warp span{
-    display: inline-block;
-    float: left;
-  }
+.subscription-warp span {
+  display: inline-block;
+  float: left;
+}
 
-  .subscription-ident{
-    line-height: 30px;
-    width: 3px;
-    height: calc(80% - 10px);
-    margin: 6px;
-  }
+.subscription-ident {
+  line-height: 30px;
+  width: 3px;
+  height: calc(80% - 10px);
+  margin: 6px;
+}
 
-  .subscription-warp .subscription-content{
-    opacity: 1;
-    font-size: small;
-    width: 80%;
-    height: 100%;
-  }
+.subscription-warp .subscription-content {
+  opacity: 1;
+  font-size: small;
+  width: 80%;
+  height: 100%;
+}
 
-  .subscription-delete{
-    color: #ee0d0d;
-    font-size: large;
-    width: 20px;
-    margin-top: 10px;
-    margin-left: 5px;
-  }
+.subscription-remove {
+  display: inline-block;
+  position: relative;
+  width: 15px;
+  height: 15px;
+  border-radius: 12px;
+  background-color: #da4f49;
+  font-size: 15px;
+  color: #f1f3f1;
+  line-height: 15px;
+  top: 35%;
+  right: -20px;
+  z-index: 10;
+}
+
+/deep/
+.el-checkbox__inner{
+  border-radius: 10px;
+}
 </style>
